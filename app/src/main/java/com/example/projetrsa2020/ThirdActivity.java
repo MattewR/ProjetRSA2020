bb9351456905790c97f1dd8008a209baa1251fdd
jhoule42 <jphoule29@hotmail.com>
2020-12-05 08:54:16 -0500

    Update JP - 5 dec

--- app/src/main/java/com/example/projetrsa2020/ThirdActivity.java
+++ app/src/main/java/com/example/projetrsa2020/ThirdActivity.java
@@ -1,6 +1,10 @@
 package com.example.projetrsa2020;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,28 +13,50 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ThirdActivity  extends AppCompatActivity {
+    public class ThirdActivity  extends AppCompatActivity {
+
+        private EditText valeurC;
+        private EditText valeurD;
+        private EditText valeurN;
+        private Button buttonDecrypter;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_decripter);   // Layout XML lié à cette Activity
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_decripter);   // Layout XML lié à cette Activity
+
+
+        // Pour simplifier, je reprends chaque élément graphique et les lie dans le code.
+        valeurC = findViewById(R.id.editTextC);
+        valeurD = findViewById(R.id.editTextD);
+        valeurN = findViewById(R.id.editTextN);
+        buttonDecrypter = findViewById(R.id.button_decrypter);
+
+
+
+        // Quand on click sur le bouton
+        buttonDecrypter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Appeller fonction Factoriser
+                ArrayList<Double> facteurs = factorisation(Integer.parseInt(valeurC.getText().toString()));
+
+
+
+            }
+        });
+
+
+
 
 
     }
 
 
-
-
-
-
-
-
-
-
-
-    // Code 2 du projet de Math pour générer p et q (nombres premiers)
-    public ArrayList<Double> factorisation(int n){
+        // Code 2 du projet de Math pour générer p et q (nombres premiers)
+    public ArrayList<Double> factorisation(double n){
 
         ArrayList<Double> prime_numbers = new ArrayList<Double>();
         double p = Math.floor(Math.sqrt(n));
