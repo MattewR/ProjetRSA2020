368f64b0f997d6bc5fbcd53f59ef89891de32830
jhoule42 <jphoule29@hotmail.com>
2020-12-07 15:12:45 -0500

    Ajout de javadoc - 7 dec

--- app/src/main/java/com/example/projetrsa2020/MainActivity.java
+++ app/src/main/java/com/example/projetrsa2020/MainActivity.java
@@ -536,7 +536,7 @@ public class MainActivity extends AppCompatActivity {
 
         while (r_prime != 0) {
 
-            double q = Math.floor(r_prime);
+            double q = Math.floor(r / r_prime);
 
             r = r_prime;
             x = x_prime;
